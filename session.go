8754bd90ae0a25e9697ab9999ed0c1b3ecdf8ac0
Josh Komoroske <jdkomo@gmail.com>
2017-09-27 12:17:26 -0700

    Create credentials and session from environment variables (#5)

--- session.go
+++ session.go
@@ -4,7 +4,37 @@
 
 package posh
 
+import (
+	"fmt"
+	"os"
+)
+
 type Session struct {
 	UserID string `json:"user-id"`
 	Token  string `json:"token"`
+}
+
+func NewSession(userID string, token string) *Session {
+	return &Session{
+		userID,
+		token,
+	}
+}
+
+func NewEnvSession() (*Session, error) {
+
+	userID, found := os.LookupEnv(UserIDEnvironmentVariable)
+	if !found {
+		return nil, fmt.Errorf("no %s found in environment", UserIDEnvironmentVariable)
+	}
+
+	token, found := os.LookupEnv(TokenEnvironmentVariable)
+	if !found {
+		return nil, fmt.Errorf("no %s found in environment", TokenEnvironmentVariable)
+	}
+
+	return &Session{
+		userID,
+		token,
+	}, nil
 }
